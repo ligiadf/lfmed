9f654b84df60dcdb31d95bb65aae409b31752a1f
ligiadf <39387852+ligiadf@users.noreply.github.com>
2018-08-26 10:24:30 -0300

    Config genérico

--- config.sample.php
+++ config.sample.php
@@ -1,10 +1,10 @@
 <?php
 
-define("BASE_URL", "http://localhost:8080/lfmed/");
+define("BASE_URL", "http://caminho.do/seu-projeto/");
 define("NAME", "Sistema LFMED by LFreitas");
 define("VERSION", "0.3");
 
-define("NOME_CLINICA", "Clínica Oftorrino");
+define("NOME_CLINICA", "Nome da Clínica");
 
 require 'db.php';
 

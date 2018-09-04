d82c271ce0d2fcd63f0ed2398ae47e293e5e260a
ligiadf <39387852+ligiadf@users.noreply.github.com>
2018-09-03 21:19:55 -0300

    Incluído BASE_URL e FPDF

--- config.sample.php
+++ config.sample.php
@@ -1,13 +1,15 @@
 <?php
 
+define("BASE_URL", "http://caminho-da-sua/instalacao/");
 define("NAME", "<a href=\"https://github.com/ligiadf/lfmed\" target=\"_blank\">Sistema LFMED</a>");
 define("VERSION", "0.4");
 define("DEV", "<a href=\"http://lfreitas.info\" target=\"_blank\">LFreitas</a>");
 define("LICENSE", "GNU General Public License v3.0");
 
-
 define("NOME_CLINICA", "Nome da Clínica");
 
-require 'db.php';
+require ("db.php");
+
+require (BASE_URL."assets/fpdf/fpdf.php");
 
 ?>

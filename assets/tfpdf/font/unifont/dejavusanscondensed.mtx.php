3eb84a4a506bf37c843782015b81f6541f8a9f73
ligiadf <39387852+ligiadf@users.noreply.github.com>
2018-09-07 17:01:41 -0300

    Corrigir caminho fonte FPDF.

--- assets/tfpdf/font/unifont/dejavusanscondensed.mtx.php
+++ assets/tfpdf/font/unifont/dejavusanscondensed.mtx.php
@@ -13,7 +13,7 @@ $desc=array (
 );
 $up=-63;
 $ut=44;
-$ttffile='J:\xampp\htdocs\lfmed\assets\tfpdf/font/unifont/DejaVuSansCondensed.ttf';
+$ttffile='assets/tfpdf/font/unifont/DejaVuSansCondensed.ttf';
 $originalsize=680264;
 $fontkey='dejavu';
 ?>
